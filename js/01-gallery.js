a26b140deb1762443cb83c84cc0cddbd37904b4d
DudaYura <YurkaD@ukr.net>
2022-08-10 13:14:18 +0300

    сделал 7 домашку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkupRef = createGalleryMarkup(galleryItems);
 
+galleryRef.innerHTML = galleryMarkupRef;
+galleryRef.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') return;
+
+  const originalUrl = getOriginalUrl(evt.target);
+
+  openOriginalImg(originalUrl);
+}
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    />
+    </a>
+    </div>`
+    )
+    .join('');
+}
+
+function getOriginalUrl(elem) {
+  return elem.dataset.source;
+}
+
+function openOriginalImg(originalUrl) {
+  const originalImg = basicLightbox.create(`<img src="${originalUrl}">`, {
+    onShow: () => document.addEventListener('keydown', modalCloseWithEsc),
+    onClose: () => document.removeEventListener('keydown', modalCloseWithEsc),
+  });
+  originalImg.show();
+  document.openedModal = originalImg;
+}
+
+function modalCloseWithEsc(evt) {
+  if (evt.code === 'Escape') {
+    document.openedModal.close();
+    delete document.openedModal;
+  }
+}
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryRef = document.querySelector('.gallery');
+const galleryMarcupRef = createGalleryMarkup(galleryItems);
+
+galleryRef.innerHTML = galleryMarcupRef;
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" 
+          alt="${description}" />
+        </a>`
+    )
+    .join('');
+}
+
 console.log(galleryItems);
